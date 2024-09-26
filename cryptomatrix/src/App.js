5d22a9ed7139794f662cc24a12e45ad61e6d53b4
Alish Jain <alishjain@Alishs-MBP.attlocal.net>
2024-09-26 14:57:11 -0400

    Alish main branch created

--- cryptomatrix/src/App.js
+++ cryptomatrix/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is the Cryptomatrix project. It is a project to learn about blockchain and cryptocurrencies.
         </p>
         <a
           className="App-link"
