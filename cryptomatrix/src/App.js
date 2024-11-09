4b60934bcdd306f74cc88907c33778555e70e8c1
daoVu9 <vadao.ityu@gmail.com>
2024-11-09 12:46:46 -0500

    test commit

--- cryptomatrix/src/App.js
+++ cryptomatrix/src/App.js
@@ -2,16 +2,50 @@ import React from 'react';
 import './i18n';
 import './App.css';
 import Header from './components/Header/Header';
-/*import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';*/
+import Landing from './components/Landing/Landing';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import Carousel from './components/Carousel/Carousel';
+import CryptoViewToggle from './components/CryptoViewToggle/CryptoViewToggle';
+import { BrowserRouter as Router, Routes, Route, useRoutes } from 'react-router-dom'; // Import useRoutes
+import CoinPage from './components/CoinPage/CoinPage'; // Import CoinPage
 
+function AppRoutes() {
+  // Define routes using useRoutes
+  const routes = [
+    {
+      path: "/coin/:id",
+      element: <CoinPage /> // Render CoinPage for /coin/:id route
+    }
+  ];
+
+  // Pass routes to useRoutes hook
+  return useRoutes(routes);
+}
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      {/* Header will be displayed on all pages */}
       <Header />
-      {/* Other content of your app */}
-    </div>
+
+      <Routes>
+        {/* Main landing page components */}
+        <Route 
+          path="/" 
+          element={
+            <>
+              <Landing />
+              <Carousel />
+              <CryptoViewToggle />
+            </>
+          } 
+        />
+
+        {/* Route for CoinPage - displays only when URL is /coin/:id */}
+        <Route path="/coin/:id" element={<CoinPage />} />
+      </Routes>
+    </Router>
   );
 }
 
